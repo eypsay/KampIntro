9225a958fcc1b8d778404709dce39b667beec844
eyp <eypsay@yahoo.com>
2021-01-21 00:40:35 +0300

    get metodu eklendi

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,16 +7,28 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            //
-            string[] isimler = new string[] {"eyyüp","semra","yunus" };
-            Console.WriteLine(isimler[0]);
-            Console.WriteLine(isimler[1]);
-            Console.WriteLine(isimler[2]);
+            ////
+            //string[] isimler = new string[] {"eyyüp","semra","yunus","zehra" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
             //isimler =new string[5] dediğimde isimlerin adresini değiştirdiğim için üsteki diziyi kaybederim
             //isimler[3] = "safiye";
             //Console.WriteLine(isimler[3]);
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);//burası bos gelir cunku atama yapılmadi
 
-            //List<>
+            List<string> isimler2 = new List<string> { "eyyüp", "semra", "yunus","zehra" };// List<string> isimler2 = new List<string>();
+            Console.WriteLine(isimler2[0]);
+            
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("safiye");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+
+
         }
     }
 }
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("eyyup");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("semra");
+            Console.WriteLine(isimler.Length);
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
