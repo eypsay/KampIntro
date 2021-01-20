09e6b48687f29aaf37aa36ebda099c5a7c2d9e34
eyp <eypsay@yahoo.com>
2021-01-20 22:44:33 +0300

    collections eklendi

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            string[] isimler = new string[] {"eyyüp","semra","yunus" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            //isimler =new string[5] dediğimde isimlerin adresini değiştirdiğim için üsteki diziyi kaybederim
+            //isimler[3] = "safiye";
+            //Console.WriteLine(isimler[3]);
+
+            //List<>
+        }
+    }
+}
