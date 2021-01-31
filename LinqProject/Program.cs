6ffb39fd83ef3f424679cbfff33748161fc7a940
eyp <eypsay@yahoo.com>
2021-01-31 15:10:27 +0300

    linq devam

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqProject
 {
@@ -6,7 +8,66 @@ namespace LinqProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Category> categories = new List<Category>
+            {
+                new Category {CategoryId=1,CategoryName="Bilgisayar"},
+                new Category {CategoryId=2,CategoryName="Telefon"},
+            };
+
+            List<Product> products = new List<Product> {
+            new Product  {ProdcutId =1,CategoryId=1,ProductName="Asus",
+            QuantityPerUnit ="32 Gb Ram",UnitInStock =100,UnitPrice=5000
+            },
+            new Product  {ProdcutId =2,CategoryId=1,ProductName="HP",
+            QuantityPerUnit ="8 Gb Ram",UnitInStock =10,UnitPrice=7500
+            },
+            new Product  {ProdcutId =3,CategoryId=1,ProductName="Acer",
+            QuantityPerUnit ="4 Gb Ram",UnitInStock =20,UnitPrice=3000
+            },
+            new Product  {ProdcutId =4,CategoryId=2,ProductName="Samsung",
+            QuantityPerUnit ="4 Gb Ram",UnitInStock =50,UnitPrice=7000
+            },
+            new Product  {ProdcutId =5,CategoryId=2,ProductName="APPLE",
+            QuantityPerUnit ="8 Gb Ram",UnitInStock =0,UnitPrice=10000
+            },
+            };
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitInStock > 3)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+            Console.WriteLine("-----LINQ------");
+            var result = products.Where(p => p.UnitPrice > 5000 && p.UnitInStock > 3);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ProductName);
+            }
+            Console.WriteLine("----USE METHOD----");
+            getProducts(products);
+            Console.WriteLine("LINQ USED METHOD");
+            foreach (var item in getProductsLinq(products))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+        static List<Product> getProducts(List<Product> products)
+        {
+            List<Product> filteredProduct = new List<Product>();
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 5000 && product.UnitInStock > 3)
+                {
+                    filteredProduct.Add(product);
+                }
+            }
+            return filteredProduct;
+        }
+
+        static List<Product> getProductsLinq(List<Product> products)
+        {
+            return products.Where(p => p.UnitPrice > 5000 && p.UnitInStock > 3).ToList();
         }
     }
     class Product
